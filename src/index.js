cc98d7b474167295a7f660461a7c0d0f832912a8
dikair <irzian.dika@gmail.com>
2022-08-01 06:14:14 +0700

    cek

--- src/index.js
+++ src/index.js
@@ -1,33 +1,28 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Cart from "./routes/cart";
 import App from "./App";
 import Login from "./routes/login";
 import LandingPage from "./routes/landingPage";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="cart" element={<Cart />} />
-
-        <Route path="Login" element={<Login />} />
-        <Route
-          path="*"
-          element={
-            <main style={{ padding: "1rem" }}>
-              {" "}
-              <p>There's nothing here!</p>{" "}
-            </main>
-          }
-        />
-      </Route>
-    </Routes>
+  <Routes>
+  <Route path="/" element={<App/>}>
+  <Route path="cart" element={<Cart />} />
+  
+  <Route path="Login" element={<Login />} />
+  <Route path="*" element={ <main style={{ padding: "1rem" }}> <p>There's nothing here!</p> </main>}/>
+  </Route>
+  </Routes>
   </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
