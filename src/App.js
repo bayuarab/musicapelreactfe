d705f9ca1e471040b08b2f6ee517f9030663c203
dikair <irzian.dika@gmail.com>
2022-08-01 05:34:59 +0700

    bismillah

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import ButtonAppBar from './component/websiteAppBar';
 import { AppBar, Box } from '@mui/material';
 import HeaderBar from './component/websiteAppBar';
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="base">
       <Box sx={{ flexGrow: 1 }}><HeaderBar/></Box>
+    <div className="contentWrapper">
+    <Outlet />
+    </div>
     </div>
   );
   
---
+++ src/routes/detailCourse.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import React from "react";
+export default function DetailCourse(){
+    return(
+            <Box style={{
+                width: '100%',
+                height: '400px',
+                margin:'0px auto auto 0px'
+            }}>
+                cek
+            </Box>
+    )
+}
