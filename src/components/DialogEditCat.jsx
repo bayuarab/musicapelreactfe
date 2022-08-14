63a4afdc9f605efe044a205990b75b640e3cee98
Bayu Aji Dwisanjaya <arabdoi88@gmail.com>
2022-08-14 14:33:54 +0700

    Add snackbar on admin manage user and category

--- src/components/DialogEditCat.jsx
+++ src/components/DialogEditCat.jsx
@@ -1,132 +1,231 @@
-import React, { useState } from "react";
-import { Box, Button, Dialog, TextField, Grid, DialogTitle, DialogContent, Input, Stack, Snackbar, Alert } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Grid,
+  Input,
+  Snackbar,
+  Stack,
+  TextField,
+} from "@mui/material";
 import axios from "axios";
+import React, { useState } from "react";
 
 function DialogEditCat(
-	props = {
-		open: false,
-		id: 0,
-		onClose: () => {},
-		onAdd: () => {},
-	}
+  props = {
+    open: false,
+    id: 0,
+    onClose: () => {},
+    onAdd: () => {},
+  }
 ) {
-	const [id, setId] = useState("");
-	const [categoryName, setcategoryName] = useState("");
-	const [categoryDescription, setcategoryDescription] = useState("");
-	const [imagePreview, setImagePreview] = useState("");
-	const [base64, setBase64] = useState("");
-	const [err, setErr] = useState("");
-	const [open, setOpen] = React.useState(false);
+  const [id, setId] = useState("");
+  const [categoryName, setcategoryName] = useState("");
+  const [categoryDescription, setcategoryDescription] = useState("");
+  const [imagePreview, setImagePreview] = useState("");
+  const [base64, setBase64] = useState("");
+  const [err, setErr] = useState("");
+  const [open, setOpen] = React.useState(false);
+  const [severityType, setSeverityType] = useState("error");
 
-	const Alerts = React.forwardRef(function Alerts(props, ref) {
-		return <Alert elevation={6} ref={ref} variant="filled" {...props} />;
-	});
+  const Alerts = React.forwardRef(function Alerts(props, ref) {
+    return <Alert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
 
-	const handleClose = (event, reason) => {
-		if (reason === "clickaway") {
-			return;
-		}
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
 
-		setOpen(false);
-	};
+    setOpen(false);
+  };
 
-	/* Methods to convert image input into base64 */
-	const onFileSubmit = (e) => {
-		e.preventDefault();
-		console.log(base64);
-	};
-	const onChange = (e) => {
-		console.log("file", e.target.files[0]);
-		let file = e.target.files[0];
-		if (file) {
-			const reader = new FileReader();
-			reader.onload = _handleReaderLoaded;
-			reader.readAsBinaryString(file);
-		}
-	};
+  /* Methods to convert image input into base64 */
+  const onFileSubmit = (e) => {
+    e.preventDefault();
+    console.log(base64);
+  };
+  const onChange = (e) => {
+    console.log("file", e.target.files[0]);
+    let file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = _handleReaderLoaded;
+      reader.readAsBinaryString(file);
+    }
+  };
 
-	const _handleReaderLoaded = (readerEvt) => {
-		let binaryString = readerEvt.target.result;
-		setBase64(btoa(binaryString));
-	};
-	const photoUpload = (e) => {
-		e.preventDefault();
-		const reader = new FileReader();
-		const file = e.target.files[0];
-		console.log("reader", reader);
-		console.log("file", file);
-		if (reader !== undefined && file !== undefined) {
-			reader.onloadend = () => {
-				setImagePreview(reader.result);
-			};
-			reader.readAsDataURL(file);
-		}
-	};
+  const _handleReaderLoaded = (readerEvt) => {
+    let binaryString = readerEvt.target.result;
+    setBase64(btoa(binaryString));
+  };
+  const photoUpload = (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    console.log("reader", reader);
+    console.log("file", file);
+    if (reader !== undefined && file !== undefined) {
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
-	/* Method to POST new Brand Item */
-	const postKelas = () => {
-		const postDataa = { id: id, category: categoryName, image: base64, desc: categoryDescription };
-		console.log(postDataa);
-		axios
-			.put("https://localhost:7132/api/CourseCategory", postDataa)
-			.then((res) => {
-				if (res.status === 200) {
-					console.log(res.status);
-					console.log(res.data);
-					props.onClose();
-				}
-			})
-			.catch((err) => {
-				console.log(err.response.data);
-				setErr("Error : Kategori Tidak Valid");
-				setOpen(true);
-			});
-	};
+  /* Method to POST new Brand Item */
+  const postKelas = () => {
+    const postDataa = {
+      id: id,
+      category: categoryName,
+      image: base64,
+      desc: categoryDescription,
+    };
+    console.log(postDataa);
+    axios
+      .put("https://localhost:7132/api/CourseCategory", postDataa)
+      .then((res) => {
+        if (res.status === 200) {
+          console.log(res.status);
+          console.log(res.data);
+          setSeverityType("success");
+          setErr("Berhasil mengubah kategori");
+          setOpen(true);
+          props.onClose();
+        }
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+        setErr("Error : Kategori Tidak Valid");
+        setOpen(true);
+      });
+  };
 
-	return (
-		<div>
-			<Dialog open={props.open} onClose={props.onClose}>
-				<div style={{ padding: "20px", width: "100%" }}>
-					{/* TITLE */}
-					<DialogTitle>Tambahkan Kelas Baru</DialogTitle>
-					<DialogContent>
-						{/* FORM INPUT */}
-						<form onSubmit={(e) => onFileSubmit(e)} onChange={(e) => onChange(e)}>
-							{imagePreview === "" ? "" : <img style={{ width: "100%", height: "100%" }} src={imagePreview} alt="upload" />}
-							<Input type="file" name="avatar" id="file" accept=".jpef, .png, .jpg" onChange={photoUpload} src={imagePreview} />
-						</form>
+  return (
+    <div>
+      <Dialog open={props.open} onClose={props.onClose}>
+        <div style={{ padding: "20px", width: "100%" }}>
+          {/* TITLE */}
+          <DialogTitle>Tambahkan Kelas Baru</DialogTitle>
+          <DialogContent>
+            {/* FORM INPUT */}
+            <form
+              onSubmit={(e) => onFileSubmit(e)}
+              onChange={(e) => onChange(e)}
+            >
+              {imagePreview === "" ? (
+                ""
+              ) : (
+                <img
+                  style={{ width: "100%", height: "100%" }}
+                  src={imagePreview}
+                  alt="upload"
+                />
+              )}
+              <Input
+                type="file"
+                name="avatar"
+                id="file"
+                accept=".jpef, .png, .jpg"
+                onChange={photoUpload}
+                src={imagePreview}
+              />
+            </form>
 
-						<form
-							onSubmit={(e) => {
-								e.preventDefault();
-								postKelas();
-							}}>
-							<Grid columnGap="10px" justifyContent="center" style={{ paddingBottom: "10px" }}>
-								<Grid>
-									<Box noValidate>
-										<TextField id="id" value={id} label="Id Kategori" onChange={(e) => setId(e.target.value)} style={{ display: "flex", flexGrow: 1, marginTop: "20px", marginBottom: "20px" }} />
-										<TextField id="name" value={categoryName} label="Nama Kategori" onChange={(e) => setcategoryName(e.target.value)} style={{ display: "flex", flexGrow: 1, marginTop: "20px", marginBottom: "20px" }} />
-										<TextField id="description" value={categoryDescription} label="Deskripsi Kategori" onChange={(e) => setcategoryDescription(e.target.value)} style={{ display: "flex", flexGrow: 1, marginTop: "20px", marginBottom: "20px" }} />
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                postKelas();
+              }}
+            >
+              <Grid
+                columnGap="10px"
+                justifyContent="center"
+                style={{ paddingBottom: "10px" }}
+              >
+                <Grid>
+                  <Box noValidate>
+                    <TextField
+                      id="id"
+                      value={id}
+                      label="Id Kategori"
+                      onChange={(e) => setId(e.target.value)}
+                      style={{
+                        display: "flex",
+                        flexGrow: 1,
+                        marginTop: "20px",
+                        marginBottom: "20px",
+                      }}
+                    />
+                    <TextField
+                      id="name"
+                      value={categoryName}
+                      label="Nama Kategori"
+                      onChange={(e) => setcategoryName(e.target.value)}
+                      style={{
+                        display: "flex",
+                        flexGrow: 1,
+                        marginTop: "20px",
+                        marginBottom: "20px",
+                      }}
+                    />
+                    <TextField
+                      id="description"
+                      value={categoryDescription}
+                      label="Deskripsi Kategori"
+                      onChange={(e) => setcategoryDescription(e.target.value)}
+                      style={{
+                        display: "flex",
+                        flexGrow: 1,
+                        marginTop: "20px",
+                        marginBottom: "20px",
+                      }}
+                    />
 
-										<Button disabled={id === "" || categoryName === "" || categoryDescription === "" || base64 === "" ? true : false} type="submit" fullWidth variant="contained" style={{ display: "flex", flexGrow: 1, marginTop: "20px", marginBottom: "20px" }}>
-											Edit Kategori
-										</Button>
-									</Box>
-								</Grid>
-							</Grid>
-						</form>
-					</DialogContent>
-				</div>
-			</Dialog>
-			<Stack spacing={2} sx={{ width: "100%" }}>
-				<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-					<Alerts onClose={handleClose} severity="error" sx={{ width: "100%" }}>
-						{err}
-					</Alerts>
-				</Snackbar>
-			</Stack>
-		</div>
-	);
+                    <Button
+                      disabled={
+                        id === "" ||
+                        categoryName === "" ||
+                        categoryDescription === "" ||
+                        base64 === ""
+                          ? true
+                          : false
+                      }
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      style={{
+                        display: "flex",
+                        flexGrow: 1,
+                        marginTop: "20px",
+                        marginBottom: "20px",
+                      }}
+                    >
+                      Edit Kategori
+                    </Button>
+                  </Box>
+                </Grid>
+              </Grid>
+            </form>
+          </DialogContent>
+        </div>
+      </Dialog>
+      <Stack spacing={2} sx={{ width: "100%" }}>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alerts
+            onClose={handleClose}
+            severity={severityType}
+            sx={{ width: "100%" }}
+          >
+            {err}
+          </Alerts>
+        </Snackbar>
+      </Stack>
+    </div>
+  );
 }
 
 export default DialogEditCat;
--- src/pages/admin/manageKelas.js
+++ src/pages/admin/manageKelas.js
@@ -1,5 +1,6 @@
 import SearchIcon from "@mui/icons-material/Search";
 import {
+  Alert,
   Box,
   Button,
   Card,
@@ -8,6 +9,8 @@ import {
   Container,
   Grid,
   Paper,
+  Snackbar,
+  Stack,
   TextField,
   Toolbar,
   Typography,
@@ -44,6 +47,10 @@ const theme = createTheme({
   },
 });
 
+const Alerts = React.forwardRef(function Alerts(props, ref) {
+  return <Alert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 function ManageKelas() {
   /* useStates dan metode-metode untuk keperluan GET daftar semua merk */
   const [refreshPage, setRefreshPage] = useState(false);
@@ -52,6 +59,17 @@ function ManageKelas() {
   // const [category, setCat] = useState([]);
   const [selectedCat, setSelectedCat] = useState({});
   const [openDelete, setOpenDelete] = useState(false);
+  const [severityType, setSeverityType] = useState("error");
+  const [err, setErr] = useState("");
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const handleCloseDelete = (state) => {
     if (!state) return setOpenDelete(false);
@@ -60,12 +78,18 @@ function ManageKelas() {
         const response = await api.delete(`/${selectedCat.id}`);
         console.log(response.data);
         getListOfBrands();
+        setSeverityType("warning");
+        setErr("Kategori telah dihapus dari daftar");
+        setOpen(true);
       } catch (err) {
         !err.response
           ? console.log(`Error: ${err.message}`)
           : console.log(err.response.data);
         console.log(err.response.status);
         console.log(err.response.headers);
+        setSeverityType("danger");
+        setErr("Error: Gagal menghapus, terjadi kesalahan");
+        setOpen(true);
       }
     };
     fetchDelete();
@@ -121,170 +145,188 @@ function ManageKelas() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        {/* Header bar */}
-        <HeaderSet roles={`admin`} />
+    <div>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ display: "flex" }}>
+          <CssBaseline />
+          {/* Header bar */}
+          <HeaderSet roles={`admin`} />
 
-        {/* Body Content */}
-        <Box
-          component="main"
-          sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light"
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: "100vh",
-            overflow: "auto",
-          }}
-        >
-          <Toolbar />
-
-          {/* DIALOG ADD*/}
-          <DialogAddKelas
-            open={openAdd}
-            onClose={() => {
-              setOpenAdd(false);
-              setRefreshPage((status) => !status);
+          {/* Body Content */}
+          <Box
+            component="main"
+            sx={{
+              backgroundColor: (theme) =>
+                theme.palette.mode === "light"
+                  ? theme.palette.grey[100]
+                  : theme.palette.grey[900],
+              flexGrow: 1,
+              height: "100vh",
+              overflow: "auto",
             }}
-          />
+          >
+            <Toolbar />
 
-          {/* DIALOG EDIT */}
-          <DialogEditCat
-            open={openEdit}
-            onClose={() => {
-              setOpenEdit(false);
-              setRefreshPage((status) => !status);
-            }}
-          />
-          {/* <ManageBrandDialogEditItem open={openEdit} editItemData={editItemData} onClose={() => { setOpenEdit(false); setRefreshPage((status) => !status); }} /> */}
+            {/* DIALOG ADD*/}
+            <DialogAddKelas
+              open={openAdd}
+              onClose={() => {
+                setOpenAdd(false);
+                setRefreshPage((status) => !status);
+              }}
+            />
 
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            <Grid container spacing={3}>
-              <Grid item xs={12}>
-                <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
-                  {/* TITLE */}
-                  <Typography
-                    variant="h5"
-                    color="secondary"
-                    style={{ fontWeight: "bold" }}
+            {/* DIALOG EDIT */}
+            <DialogEditCat
+              open={openEdit}
+              onClose={() => {
+                setOpenEdit(false);
+                setRefreshPage((status) => !status);
+              }}
+            />
+            {/* <ManageBrandDialogEditItem open={openEdit} editItemData={editItemData} onClose={() => { setOpenEdit(false); setRefreshPage((status) => !status); }} /> */}
+
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+              <Grid container spacing={3}>
+                <Grid item xs={12}>
+                  <Paper
+                    sx={{ p: 2, display: "flex", flexDirection: "column" }}
                   >
-                    Manage Kategori Kursus
-                  </Typography>
-
-                  {/* BOX PENCARIAN DATA */}
-                  <div style={{ display: "flex", padding: "20px 0" }}>
-                    <TextField
-                      value={searchQuery}
-                      onChange={(e) => setSearchQuery(e.target.value)}
-                      id="input-with-icon-textfield"
-                      label="Pencarian Berdasarkan Nama atau Id Kategori"
-                      InputProps={{
-                        endAdornment: <SearchIcon color="primary" />,
-                      }}
-                      variant="outlined"
-                      style={{
-                        display: "flex",
-                        flexGrow: 1,
-                        marginRight: "10px",
-                      }}
-                    />
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={() => {
-                        setOpenAdd(true);
-                      }}
-                      style={{ width: "auto", color: "white" }}
+                    {/* TITLE */}
+                    <Typography
+                      variant="h5"
+                      color="secondary"
+                      style={{ fontWeight: "bold" }}
                     >
-                      Tambah Baru
-                    </Button>
-                  </div>
+                      Manage Kategori Kursus
+                    </Typography>
 
-                  {kategoriFilter().map((item) => (
-                    <Card key={item.id} style={{ margin: "2% 0" }}>
-                      <Grid container spacing={3}>
-                        <Grid item xs={12} md={2}>
-                          <CardMedia
-                            component="img"
-                            style={{ objectFit: "contain" }}
-                            height="100"
-                            image={`data:image/jpeg;base64,${item.image}`}
-                            alt="Image"
-                          />
-                        </Grid>
-                        <Grid item xs={12} md={10}>
-                          <CardContent>
-                            <Typography
-                              gutterBottom
-                              variant="h5"
-                              component="div"
-                            >
-                              {item.category}
-                            </Typography>
-                            <Typography variant="h7" color="div">
-                              Id {item.id}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                              {item.desc}
-                            </Typography>
+                    {/* BOX PENCARIAN DATA */}
+                    <div style={{ display: "flex", padding: "20px 0" }}>
+                      <TextField
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                        id="input-with-icon-textfield"
+                        label="Pencarian Berdasarkan Nama atau Id Kategori"
+                        InputProps={{
+                          endAdornment: <SearchIcon color="primary" />,
+                        }}
+                        variant="outlined"
+                        style={{
+                          display: "flex",
+                          flexGrow: 1,
+                          marginRight: "10px",
+                        }}
+                      />
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={() => {
+                          setOpenAdd(true);
+                        }}
+                        style={{ width: "auto", color: "white" }}
+                      >
+                        Tambah Baru
+                      </Button>
+                    </div>
 
-                            <Grid container spacing={1}>
-                              <Grid item xs={12} md={4}>
-                                <Button
-                                  fullWidth
-                                  variant="outlined"
-                                  color="primary"
-                                  component={Link}
-                                  to={`/admin/category`}
-                                >
-                                  Daftar Kelas
-                                </Button>
+                    {kategoriFilter().map((item) => (
+                      <Card key={item.id} style={{ margin: "2% 0" }}>
+                        <Grid container spacing={3}>
+                          <Grid item xs={12} md={2}>
+                            <CardMedia
+                              component="img"
+                              style={{ objectFit: "contain" }}
+                              height="100"
+                              image={`data:image/jpeg;base64,${item.image}`}
+                              alt="Image"
+                            />
+                          </Grid>
+                          <Grid item xs={12} md={10}>
+                            <CardContent>
+                              <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="div"
+                              >
+                                {item.category}
+                              </Typography>
+                              <Typography variant="h7" color="div">
+                                Id {item.id}
+                              </Typography>
+                              <Typography
+                                variant="body2"
+                                color="text.secondary"
+                              >
+                                {item.desc}
+                              </Typography>
+
+                              <Grid container spacing={1}>
+                                <Grid item xs={12} md={4}>
+                                  <Button
+                                    fullWidth
+                                    variant="outlined"
+                                    color="primary"
+                                    component={Link}
+                                    to={`/admin/category`}
+                                  >
+                                    Daftar Kelas
+                                  </Button>
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                  <Button
+                                    fullWidth
+                                    variant="outlined"
+                                    color="primary"
+                                    onClick={(e) => {
+                                      e.preventDefault();
+                                      setOpenEdit(true);
+                                      setEditItemData(item);
+                                    }}
+                                  >
+                                    Edit Kategori
+                                  </Button>
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                  <Button
+                                    fullWidth
+                                    variant="outlined"
+                                    color="primary"
+                                    onClick={() => handleClickOpenDelete(item)}
+                                  >
+                                    Hapus Kategori
+                                  </Button>
+                                </Grid>
                               </Grid>
-                              <Grid item xs={12} md={4}>
-                                <Button
-                                  fullWidth
-                                  variant="outlined"
-                                  color="primary"
-                                  onClick={(e) => {
-                                    e.preventDefault();
-                                    setOpenEdit(true);
-                                    setEditItemData(item);
-                                  }}
-                                >
-                                  Edit Kategori
-                                </Button>
-                              </Grid>
-                              <Grid item xs={12} md={4}>
-                                <Button
-                                  fullWidth
-                                  variant="outlined"
-                                  color="primary"
-                                  onClick={() => handleClickOpenDelete(item)}
-                                >
-                                  Hapus Kategori
-                                </Button>
-                              </Grid>
-                            </Grid>
-                          </CardContent>
+                            </CardContent>
+                          </Grid>
                         </Grid>
-                      </Grid>
-                    </Card>
-                  ))}
-                </Paper>
+                      </Card>
+                    ))}
+                  </Paper>
+                </Grid>
               </Grid>
-            </Grid>
-          </Container>
+            </Container>
+          </Box>
+          <DialogDeleteCat
+            selectedCat={selectedCat}
+            logState={openDelete}
+            onClose={handleCloseDelete}
+          />
         </Box>
-        <DialogDeleteCat
-          selectedCat={selectedCat}
-          logState={openDelete}
-          onClose={handleCloseDelete}
-        />
-      </Box>
-    </ThemeProvider>
+      </ThemeProvider>
+      <Stack spacing={2} sx={{ width: "100%" }}>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alerts
+            onClose={handleClose}
+            severity={severityType}
+            sx={{ width: "100%" }}
+          >
+            {err}
+          </Alerts>
+        </Snackbar>
+      </Stack>
+    </div>
   );
 }
 
