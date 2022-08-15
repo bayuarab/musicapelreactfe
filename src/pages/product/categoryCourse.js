9e48451bddfb4c4471a46f220f70926250a16d78
Bara Priamorta <barapriamorta98@gmail.com>
2022-08-15 08:45:33 +0700

    fix image course detail

--- src/pages/product/categoryCourse.js
+++ src/pages/product/categoryCourse.js
@@ -337,7 +337,7 @@ export default function CategoryCourse() {
 							height: "400px",
 						}}>
 						<img
-							src={`${detailOfACourse.courseImage}`}
+							src={`data:image/jpeg;base64,${detailOfACourse.courseImage}`}
 							width="75%"
 							alt={detailOfACourse.courseImage}
 							style={{
