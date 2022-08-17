c99b9f596c8df4fca00baf72572d45af0b7c43fe
Bara Priamorta <barapriamorta98@gmail.com>
2022-08-18 01:56:24 +0700

    Responsive landing page, stepper fact

--- src/pages/home/components/Class.jsx
+++ src/pages/home/components/Class.jsx
@@ -1,51 +1,49 @@
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
-import {
-  default as Box,
-  default as Typography,
-} from "@mui/material/Typography";
+import { default as Box, default as Typography } from "@mui/material/Typography";
 import React from "react";
 import CardClass from "./CardClass";
 
 const Class = () => {
-  return (
-    <Container maxWidth="100%">
-      <CssBaseline />
-      <Box
-        sx={{
-          alignItems: "center",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <Typography
-          mt="6vh"
-          sx={{
-            fontSize: {
-              lg: "24px",
-              md: "22px",
-              sm: "18px",
-              xs: "14px",
-            },
-            color: "#5D5FEF",
-            fontWeight: "600",
-          }}
-        >
-          Explore kelas favorit
-        </Typography>
-      </Box>
-      <center>
-        <Box
-          mt="6vh"
-          sx={{
-            width: "88%",
-          }}
-        >
-          <CardClass />
-        </Box>
-      </center>
-    </Container>
-  );
+	return (
+		<Container maxWidth="100%">
+			<CssBaseline />
+			<Box
+				sx={{
+					alignItems: "center",
+					display: "flex",
+					flexDirection: "column",
+				}}>
+				<Typography
+					mt={{
+						md: "6vh",
+						sm: "4vh",
+						xs: "3.5vh",
+					}}
+					sx={{
+						fontSize: {
+							lg: "24px",
+							md: "22px",
+							sm: "18px",
+							xs: "14px",
+						},
+						color: "#5D5FEF",
+						fontWeight: "600",
+					}}>
+					Explore kelas favorit
+				</Typography>
+			</Box>
+			<center>
+				<Box
+					mt="6vh"
+					sx={{
+						width: "88%",
+					}}>
+					<CardClass />
+				</Box>
+			</center>
+		</Container>
+	);
 };
 
 export default Class;
